3c8d936626a4e1f6e4a473c7f1cb64c857a549a0
Smdonline <4cVVgJN5dZYUhtM>
2023-11-17 15:12:01 +0100

    added new file

--- index/js/index.js
+++ index/js/index.js
@@ -1,26 +1,103 @@
-/**
- * Created by MIhai DAniel SOcianu on 11/11/2023.
- */
-
-let azi = new Date('1984-11-31');
-console.log(azi);
-class Turno {
-    inizio = "00:00";
-    fine = "00:00";
-
-    constructor(inizio, fine) {
-        this.inizio = inizio;
-        this.fine = fine;
+/*class orario in formato hh:mm*/
+class Orario {
+#ore
+    #minuti
+    constructor(ore, minuti = 0) {
+        ore = parseInt(ore)
+        minuti = parseInt(minuti)
+        if (!isNaN(ore) && ore >= 0) {
+            this.setOre(ore)
+        } else this.ore = 0
+        if (!isNaN(minuti) && minuti >= 0) {
+            this.setMinuti(minuti)
+        } else this.minuti = 0
     }
+    setMinuti(minuti) {
+        minuti = parseInt(minuti)
+        if (!isNaN(minuti)) {
+            this.minuti = minuti % 60
+        }
 
-    get_inizio() {
-        return this.inizio;
     }
+    setOre(ore) {
+        ore = parseInt(ore)
+        if (!isNaN(ore)) {
+            this.ore = ore % 24
+        }
 
-    get_fine() {
-        return this.fine;
+    }
+    fromString(stringTempo) {
+        let tempo = stringTempo.split(":", 2)
+        if (tempo) {
+            switch (tempo.length) {
+                case 1:
+                    this.setOre(parseInt(tempo[0]))
+                    break
+                case 2:
+                    this.setOre(parseInt(tempo[0]))
+                    this.setMinuti(parseInt(tempo[1]))
+                    break
+            }
+
+
+        }
     }
 }
 
-let tur = new Turno("06:00", "10:30");
-console.log(tur.inizio);
+class Durata {
+    giorni
+    ore
+    minuti
+    constructor(nOre = 0, nMinuti = 0, nGiorni = 0) {
+        nMinuti = parseInt(nMinuti)
+        nOre = parseInt(nOre)
+        nGiorni = parseInt(nGiorni)
+        if (isNaN(parseInt(nMinuti))) nMinuti = 0
+        if (isNaN(parseInt(nOre))) nOre = 0
+        if (isNaN(parseInt(nGiorni))) nGiorni = 0
+
+        this.minuti = nMinuti % 60
+        this.ore = (nOre + parseInt(nMinuti / 60)) % 24
+        this.giorni = nGiorni + (nOre + parseInt(nMinuti / 60)) / 24
+    }
+    toMilliseconds() {
+        return 1000 * (this.minuti * 60 + 3600 * this.ore + 3600 * 24 * this.giorni)
+    }
+
+}
+
+class Turno {
+#giorno
+    #inizio
+    #fine
+    #maxFineTurno
+    #maxOre = 36
+    #minTurnoOre = new Durata(2)
+
+
+    constructor(nGiorno, nInizio, nDurata) {
+        if ((nGiorno !== undefined) && (nGiorno instanceof Date)) {
+            this.giorno = nGiorno
+            this.inizio = new Date(this.giorno)
+            this.fine = new Date(this.giorno)
+        }
+        if (nInizio instanceof Durata) {
+            this.inizio.setUTCMinutes(nInizio.minuti)
+            this.inizio.setHours(this.inizio.getUTCHours() + nInizio.ore)
+            console.log(this.inizio)
+
+        }
+        if (nDurata instanceof Durata) {
+            this.fine.setUTCMinutes(nDurata.minuti)
+            this.fine.setUTCHours(this.inizio.getUTCHours() + nDurata.ore)
+        } else this.fine.setUTCHours(this.inizio.getUTCHours() + this.minTurnoOre.ore)
+    }
+}
+let inizio = new Durata(24, 15)
+let durata = new Durata(3, 15)
+let turno = new Turno(new Date('11-16-2023 GMT'), inizio, durata)
+let azi = new Date('11-16-2023 GMT')
+
+console.log(turno)
+
+
